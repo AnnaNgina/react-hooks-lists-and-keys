4126ebb26296cf4216049e9e7dcb8c0335139efd
annastacia <annastaciangina96@gmail.com>
2022-07-20 09:36:24 +0300

    commit tree ec57f6a9adb04bcddfec6198e1353a789682ecd3

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,4 +1,31 @@
+// import React from "react";
+
+// function ColorList() {
+//   const colors = [
+//     "firebrick",
+//     "rebeccapurple",
+//     "salmon",
+//     "darkslategray",
+//     "hotpink",
+  
+//   ];
+
+//  const colorElements = colors.map((color)=>{
+//   return <li key={color}style ={{color:color}}>{color}</li>;
+// });
+// return (
+//   <div>
+//     <h1>Top 5 CSS Colors</h1>
+//     <ol>
+    
+//       {colorElements}
+//     </ol>
+//   </div>
+// );
+// }
+// export default ColorList;
 import React from "react";
+import ColorItem from "./ColorItem";
 
 function ColorList() {
   const colors = [
@@ -7,20 +34,17 @@ function ColorList() {
     "salmon",
     "darkslategray",
     "hotpink",
-  
-  ];
+  ]
 
- const colorElements = colors.map((color)=>{
-  return <li key={color}style ={{color:color}}>{color}</li>;
-});
-return (
-  <div>
-    <h1>Top 5 CSS Colors</h1>
-    <ol>
-    
-      {colorElements}
-    </ol>
-  </div>
-);
+const colorElements = colors.map((color) => {
+  return (
+      <div>
+        <h1>Top 5 CSS Colors</h1>
+        <ol>
+            <ColorItem key={color} color={color} />
+        </ol>
+      </div>
+  )});
 }
+
 export default ColorList;
